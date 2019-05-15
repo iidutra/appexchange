2784916aa8779a6a16d8c6d061be8a3ab9ef7a60
iidutra <igordutra07@gmail.com>
2019-05-14 21:13:17 -0400

    testing system

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -52,6 +52,9 @@ RSpec.configure do |config|
   #
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
+  config.before(:each, type: :system) do
+    driven_by :rack_test
+  end
   config.infer_spec_type_from_file_location!
 
   # Filter lines from Rails gems in backtraces.
